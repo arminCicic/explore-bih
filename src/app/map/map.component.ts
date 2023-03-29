6634ac864b5eef7afca49c07045b1a43d1e48c33
arminCicic <armincicic@gmail.com>
2023-03-29 20:52:11 +0200

    finished map

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -11,7 +11,19 @@ export class MapComponent implements OnInit {
 
  
 
-  location = "Hotel+Europe,Sarajevo";
+  locations = [
+    { name: 'Hotel Hills Sarajevo' },
+    { name: 'Hotel Radon Plaza' },
+    { name: 'Hotel Grand Sarajevo'},
+    { name: 'White Fortress'},
+    { name: 'Avaz Twist Tower'},
+    { name: 'Sarajevo City Center'},
+    { name: 'War Childhood Museum'},
+    { name: 'Tabija Zmajevac'},
+    { name: 'Hotel Grand Sarajevo'},
+    { name: 'Hotel Grand Sarajevo'}
+  ];
+
   apiKey = "AIzaSyBbpucpFe_SpT0suMgubOt_gI4EvaZrthE";
   url!: SafeResourceUrl;
 
@@ -20,7 +32,14 @@ export class MapComponent implements OnInit {
   constructor(private sanitizer: DomSanitizer) {}
 
   ngOnInit(): void {
-    const unsafeUrl = `https://www.google.com/maps/embed/v1/place?key=${this.apiKey}&q=${this.location}`;
+    const unsafeUrl = `https://www.google.com/maps/embed/v1/place?key=${this.apiKey}&q=${this.locations[0].name}`;
+    this.url = this.sanitizer.bypassSecurityTrustResourceUrl(unsafeUrl);
+  }
+
+  changeLocation(index: number) {
+    const location = this.locations[index];
+    const query = `${location.name}`;
+    const unsafeUrl = `https://www.google.com/maps/embed/v1/place?key=${this.apiKey}&q=${query}`;
     this.url = this.sanitizer.bypassSecurityTrustResourceUrl(unsafeUrl);
   }
 
